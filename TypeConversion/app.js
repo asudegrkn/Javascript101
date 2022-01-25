2c78fcbb8323e7e7b5fa275be2789dec8216f41b
asudegrkn <asudegr@gmail.com>
2022-01-25 15:43:10 +0300

    add type conversions with javascript

---
+++ TypeConversion/app.js
@@ -0,0 +1,33 @@
+let value;
+
+//veritiplerini stringe çevirme
+
+value = String(123);
+value = String(true);
+value = String(false);
+value = String(function() {console.log()});
+value = (10).toString();
+
+
+
+//veritiplerini sayılara çevirme
+
+value = Number("123");
+value = Number(null);
+value = Number(undefined);
+value = Number("hallo function array çevrilemiyor");
+
+
+value = parseFloat("3.14");
+value = parseInt("3.14");
+
+ 
+
+const a= "hello" + 23;
+console.log(a);
+console.log(typeof a);
+
+
+
+console.log(value);
+console.log(typeof value);
