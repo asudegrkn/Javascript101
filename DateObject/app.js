69ec4450e54ec80325292869bee9213eb077cb8f
asudegrkn <asudegr@gmail.com>
2022-01-25 15:47:29 +0300

    add date object with javascript

---
+++ DateObject/app.js
@@ -0,0 +1,12 @@
+let value;
+
+const now= new Date();// şu anki zamanı almanı sağlar
+
+const date1 = new Date("2-09-2000  16:00:00");
+
+const date2 = new Date("february 9 2000");
+const date3 = new Date("02/9/2000");
+value = date2;
+
+
+console.log(value);
