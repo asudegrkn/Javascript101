a0bea0cf9fe408d6292097133253026a7d751287
asudegrkn <61904935+asudegrkn@users.noreply.github.com>
2022-01-25 16:16:43 +0300

    add Comparison Operators with js

---
+++ ComparisonOperators/app.js
@@ -0,0 +1,10 @@
+console.log(2 == 2); // true
+console.log("js" == "javascript"); //false
+console.log(2 =="2"); // true çünkü sol taraftakini de stringe çeviriyor 
+console.log(2 === "2");  //false 
+
+console.log(!(2 == 2)); // başında not işareti olduğu için false
+
+
+console.log (( 2 == 2) && ("ahmet" == "ahmet")); // iki koşulda doğruysa true /and operatörü
+console.log (( 2 == 2) || ("ahmet" == "ahmet")); // bir tanesi doğruysada true /or operatörü
