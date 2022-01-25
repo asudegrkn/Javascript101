46ab44b626e95379386495b3a9cab92a850629e6
asudegrkn <asudegr@gmail.com>
2022-01-25 16:13:43 +0300

    add conditions with js

---
+++ Conditions/app.js
@@ -0,0 +1,36 @@
+// console.log(2 == 2); // true
+// console.log("js" == "javascript"); //false
+// console.log(2 =="2"); // true çünkü sol taraftakini de stringe çeviriyor 
+// console.log(2 === "2");  //false 
+
+// console.log(!(2 == 2)); // başında not işareti olduğu için false
+
+
+// console.log (( 2 == 2) && ("ahmet" == "ahmet")); // iki koşulda doğruysa true and operatörü
+// console.log (( 2 == 2) || ("ahmet" == "ahmet")); // bir tanesi doğruysada true or operatörü
+
+// const user = "ali";
+
+// if (user == "ali")
+// {
+//     console.log("kullanıcı bulundu.");
+// }
+// else 
+// {
+//     console.log("kullanıcı bulunamadı");
+// }
+
+// const process = "5";
+// if (process === "1"){
+//     console.log("işlem 1 ");
+
+// }
+// else if ( process === "2"){
+//     console.log("işlem 2");
+// }
+// else if ( process === "3"){
+//     console.log("işlem 3");
+// }
+// else {
+//     console.log(("geçersiz işlem"));
+// }
