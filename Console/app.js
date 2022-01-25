86c1ae984f8e0837ab22857c1d0ff84ff4ff1513
asudegrkn <asudegr@gmail.com>
2022-01-25 15:44:10 +0300

    add console use with javascript

---
+++ Console/app.js
@@ -0,0 +1,17 @@
+console.log("ses vver adana");
+console.log(5454);
+console.log(true);  
+
+console.log(false);
+ 
+var a = 200;
+
+console.log(a);
+
+console.log(typeof a);
+
+
+console.warn("uyarı");
+
+console.error("sayfa bukunamadı");
+console.clear();
